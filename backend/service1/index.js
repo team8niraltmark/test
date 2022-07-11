1aad5b4621f097449bfb74fa5a2c3d89efbc037e
Nir Altmark <nir.a@team8labs.com>
2022-07-11 11:39:01 +0300

    Again ...

--- backend/service1/index.js
+++ backend/service1/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Is Sservice 1 is still the awesome!')
+  res.send('Is Sservice 1 is still the awesome! :)')
 })
 
 app.listen(port, () => {
