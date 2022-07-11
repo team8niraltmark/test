9d0cf644a41cba3808429112b2fbd9007cd0f075
Nir Altmark <nir.a@team8labs.com>
2022-07-11 11:21:37 +0300

    Lets make it work

--- backend/service1/index.js
+++ backend/service1/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Service 1 is still the best!')
+  res.send('Service 1 is still the awesome!')
 })
 
 app.listen(port, () => {
